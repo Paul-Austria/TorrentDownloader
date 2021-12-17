49b96eb0379b75dfd619fded66edf963f9f4da9c
Paul-Austria <1paulengelhardt@gmail.com>
2021-12-17 11:29:00 +0100

    Add project files.

---
+++ TorrentDownloaderHandler/DownloadHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TorrentDownloaderHandler
+{
+    public class DownloadHandler
+    {
+        public static void DownloadTorrent(string torrentLink, string path)
+        {
+
+        }
+    }
+}
