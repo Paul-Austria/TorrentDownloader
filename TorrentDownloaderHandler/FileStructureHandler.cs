16d796f2028b3a34bfce5decb3e40ef20b79dd5e
Paul Engelhardt <1paulengelhardt@gmail.com>
2021-12-18 16:36:35 +0100

    added folder-handling

--- TorrentDownloaderHandler/FileStructureHandler.cs
+++ TorrentDownloaderHandler/FileStructureHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,20 @@ namespace TorrentDownloaderHandler
 {
     public class FileStructureHandler
     {
-        
+        public static List<string> GetSubPaths(string path)
+        {
+            return Directory.GetDirectories(path).ToList();
+        }
+
+        public static List<string> AddNewFolder(string path, string folderName)
+        {
+            Directory.CreateDirectory(path +"/" + folderName);
+            return GetSubPaths(path);
+        }
+
+        public static List<string> GetFiles(string path)
+        {
+            return Directory.GetFiles(path).ToList();
+        }
     }
 }
